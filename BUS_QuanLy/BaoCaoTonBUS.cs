3101710a47ba994e1a29607960b69718d323e571
ngophukiet <19521726@gm.uit.edu.vn>
2021-06-29 21:16:41 +0700

    Bus

---
+++ BUS_QuanLy/BaoCaoTonBUS.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO_QuanLy;
+using System.Data;
+
+namespace BUS_QuanLy
+{
+    public class BaoCaoTonBUS
+    {
+        private static BaoCaoTonBUS instance;
+        private BaoCaoTonBUS() { }
+        public static BaoCaoTonBUS Instance
+        {
+            get { if (instance == null) instance = new BaoCaoTonBUS(); return instance; }
+            private set { BaoCaoTonBUS.instance = value; }
+        }
+        public DataTable KhoiTaoBaoCaoTon() { 
+            DataTable dt = PhieuSuaChuaDAO.Instance.TaoDataTable(6, new string[] { "STT", "Mã", "Vật tư phụ tùng", "Tồn đầu", "Phát sinh", "Tồn cuối" });
+            return dt;
+        }
+
+        public DataTable ThemHangBaoCaoTon(DataTable a, int ma, string vtpt, int TonDau, int PhatSinh, int TonCuoi)
+        {
+            DataRow dr = a.NewRow();
+            dr["STT"] = a.Rows.Count;
+            dr["Mã"] = ma;
+            dr["Vật tư phụ tùng"] = vtpt;
+            dr["Tồn đầu"] = TonDau;
+            dr["Phát sinh"] = PhatSinh;
+            dr["Tồn cuối"] = TonCuoi;
+            a.Rows.Add(dr);
+            return a;
+        }
+
+        public DataTable LayDtKHO()
+        {
+            return BaoCaoTonDAO.Instance.LayDtKHO();
+        }
+
+        public bool KiemTraThoiDiem(DateTime a)
+        {
+            return BaoCaoTonDAO.Instance.KiemTraThoiDiem(a);
+        }
+
+        public void TaoBaoCaoMoi(DataTable a)
+        {
+            a.Clear();
+        }
+
+        public int SoLuongNhapVao(int MaPhuTung, int Thang, int Nam)
+        {
+            return BaoCaoTonDAO.Instance.SoLuongNhapVao(MaPhuTung, Thang, Nam);
+        }
+
+        public int SoLuongBanRa(int MaPhuTung, int Thang, int Nam)
+        {
+            return BaoCaoTonDAO.Instance.SoLuongBanRa(MaPhuTung, Thang, Nam);
+        }
+
+        public int SoLuongPhatSinh(int MaPhuTung, int Thang, int Nam)
+        {
+            return SoLuongNhapVao(MaPhuTung, Thang, Nam) - SoLuongBanRa(MaPhuTung, Thang, Nam);
+        }
+
+        public int LuongTonDauThang(int MaPhuTung, int Thang, int Nam)
+        {
+            return BaoCaoTonDAO.Instance.LuongTonDauThang(MaPhuTung, Thang, Nam);
+        }
+
+        public int LuongTonCuoiThang(int MaPhuTung, int Thang, int Nam)
+        {
+            return LuongTonDauThang(MaPhuTung, Thang, Nam) + SoLuongPhatSinh(MaPhuTung, Thang, Nam);
+        }
+
+        public void NhapBaoCaoTon(DataTable a, DateTime b)
+        {
+            BaoCaoTonDAO.Instance.NhapBaoCaoTon(a, b);
+        }
+
+        public DataTable TaoBaoCaoTon(DateTime b)
+        {
+            DataTable a;
+            a = KhoiTaoBaoCaoTon();
+            DataTable dt = LayDtKHO();
+            foreach (DataRow row in dt.Rows)
+            {
+                a = ThemHangBaoCaoTon(a, int.Parse(row["MaPhuTung"].ToString()), row["TenVatTuPhuTung"].ToString(), LuongTonDauThang(int.Parse(row["MaPhuTung"].ToString()), b.Month, b.Year), SoLuongPhatSinh(int.Parse(row["MaPhuTung"].ToString()), b.Month, b.Year), LuongTonCuoiThang(int.Parse(row["MaPhuTung"].ToString()), b.Month, b.Year));
+            }
+            return a;
+        }
+
+        public DataTable TruyXuatBaoCaoTon(DateTime a)
+        {
+            DataTable dt = BaoCaoTonDAO.Instance.TruyXuatBaoCaoTon(a);
+            DataTable b = KhoiTaoBaoCaoTon();
+            foreach (DataRow row in dt.Rows)
+            {
+                b = ThemHangBaoCaoTon(b, int.Parse(row["MaPhuTung"].ToString()), row["TenVatTuPhuTung"].ToString(), int.Parse(row["TonDau"].ToString()), int.Parse(row["PhatSinh"].ToString()), int.Parse(row["TonCuoi"].ToString()));
+            }
+            return b;
+        }
+
+        public bool KiemTraDuLieuBaoCao(DateTime a)
+        {
+            return BaoCaoTonDAO.Instance.KiemTraDuLieuBaoCao(a);
+        }
+
+        public bool ThangDauTien()
+        {
+            return BaoCaoTonDAO.Instance.ThangDauTien();
+        }
+    }
+}
---
+++ BUS_QuanLy/PhieuSuaChuaBUS.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using DAO_QuanLy;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BUS_QuanLy
+{
+	public class PhieuSuaChuaBUS
+	{
+		private static PhieuSuaChuaBUS instance;
+		private PhieuSuaChuaBUS() { }
+		private DataTable VTPTDangNhap;
+		public static PhieuSuaChuaBUS Instance
+		{
+			get { if (instance == null) instance = new PhieuSuaChuaBUS(); return instance; }
+			private set { PhieuSuaChuaBUS.instance = value; }
+		}
+		public void KhoiTaoDtVTPTDangNhap()
+		{
+			VTPTDangNhap = new DataTable();
+			VTPTDangNhap = TaoDataTable(3, new string[] { "ma", "slhientai", "slchophep" });
+		}
+		public DataTable KhoiTaoDtVTPT()
+		{
+			return TaoDataTable(6, new string[] { "STT", "Vật tư phụ tùng", "Số lượng", "Đơn giá", "Thành tiền", "Mã phụ tùng" });
+		}
+
+		public DataTable KhoiTaoDtTienCong()
+		{
+			return TaoDataTable(4, new string[] { "STT", "Nội dung", "Chi phí", "Mã tiền công" });
+		}
+		public DataTable TaoDataTable(int a, string[] name)
+		{
+			return PhieuSuaChuaDAO.Instance.TaoDataTable(a, name);
+		}
+		public DataTable ThemHangVTPT(DataTable dt, string VTPT, string Soluong, int DonGia, string MaPhuTung)
+		{
+			int a = dt.Rows.Count + 1;
+			int sl = int.Parse(Soluong);
+			return PhieuSuaChuaDAO.Instance.ThemHangVTPT(dt, a, VTPT, sl, DonGia, MaPhuTung);
+		}
+
+		public DataTable ThemHangTienCong(DataTable dt, string NoiDung, int ChiPhi, string MaTC)
+		{
+			int a = dt.Rows.Count + 1;
+			return PhieuSuaChuaDAO.Instance.ThemHangTienCong(dt, a, NoiDung, ChiPhi, MaTC);
+		}
+		public int LayDonGiaVTPT(string maVTPT)
+		{
+			return PhieuSuaChuaDAO.Instance.LayDonGiaVTPT(maVTPT);
+		}
+
+		public int LayChiPhiTienCong(string maTienCong)
+		{
+			return PhieuSuaChuaDAO.Instance.LayChiPhiTienCong(maTienCong);
+		}
+
+		public string LayNoiDungTienCong(string maTienCong)
+		{
+			return PhieuSuaChuaDAO.Instance.LayNoiDungTienCong(maTienCong);
+		}
+		public void LuuPhieuSuaChua(string BienSo, int TienCong, int TienPhuTung, int TongTien, DataTable TC, DataTable VTPT)//Lưu dữ liệu được nhập vào 2 bảng PHIEUSUACHUA và CHITIETPHIEUSUACHUA
+		{
+			PhieuSuaChuaDAO.Instance.LuuPhieuSuaChua(BienSo, TienCong, TienCong, TongTien, TC, VTPT);
+
+		}
+
+		public void CapNhapVTPT(DataTable VTPT)
+		{
+			PhieuSuaChuaDAO.Instance.CapNhapVTPT(VTPT);
+		}
+		public bool KiemTraSoLuong(string MaVTPT, int SoLuong)//Kiểm tra số lượng nhập vào có nằm trong khả năng đáp ứng của kho.
+		{
+			bool TonTai = false;
+			int Tong = 0;
+			foreach (DataRow row in VTPTDangNhap.Rows)
+			{
+				if (row["ma"].ToString() == MaVTPT)
+				{
+					TonTai = true;
+					Tong = SoLuong + int.Parse(row["slhientai"].ToString());
+					if (Tong > int.Parse(row["slchophep"].ToString()))
+					{
+						return false;
+					}
+					else
+					{
+						row["slhientai"] = Tong.ToString();
+					}
+				}
+			}
+			if (!TonTai)
+			{
+				return PhieuSuaChuaDAO.Instance.KiemTraSoLuong(VTPTDangNhap, MaVTPT, SoLuong);
+			}
+			return true;
+		}
+		public void XoaDtVTPTDangNhap()
+		{
+			VTPTDangNhap.Clear();
+		}
+		public void CapNhatTienNo(string BienSo, int TienSuaChua)
+		{
+			PhieuSuaChuaDAO.Instance.CapNhatTienNo(BienSo, TienSuaChua);
+		}
+
+	}
+
+}
